6189bd079331a50f4e44bea4d6ca83d70494f077
Aleksey.tcherkasoff <136820540+Alekseytcherkasoff@users.noreply.github.com>
2026-05-18 16:21:16 +0700

    Add files via upload

---
+++ 09-props/src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import PetInfo from './components/Petinfo'
+
+function App() {
+  return (
+    <div className="App">
+      <PetInfo animal="cat" age="4" />
+    </div>
+  )
+}
+
+export default App
